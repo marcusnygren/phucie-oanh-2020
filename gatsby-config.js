9c9d776e8b8e7c8286a4977d6d87e94decdbf9ec
Marcus Nygren <marcusnygren.mn@gmail.com>
2019-11-29 12:47:48 +0100

    Added wedding idea info

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,16 +1,15 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Node.js (TypeScript) API'
+    title: 'Phucie and Oanh 2020'
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Gatsby + Node.js (TypeScript) API',
-        short_name: 'Gatbsy + Node.js (TypeScript)',
+        name: 'Phucie and Oanh 2020',
+        short_name: 'Phucie and Oanh 2020',
         start_url: '/',
-        icon: 'src/images/gatsby-icon.png'
       }
     }
   ]
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,52 +3,31 @@ import { Helmet } from 'react-helmet';
 import '../styles/index.css';
 
 function Index() {
-  const [date, setDate] = useState(null);
+  const [dateIdea, setDateIdea] = useState(null);
   useEffect(() => {
-    async function getDate() {
-      const res = await fetch('/api/date');
-      const newDate = await res.text();
-      setDate(newDate);
+    async function getDateIdea() {
+      const res = await fetch('/api/dateIdea');
+      const newDateIdea = await res.text();
+      setDateIdea(newDateIdea);
     }
-    getDate();
+    getDateIdea();
   }, []);
+
   return (
     <main>
       <Helmet>
-        <title>Gatsby + Node.js (TypeScript) API</title>
+        <title>Phucie and Oanh 2020</title>
       </Helmet>
-      <h1>Gatsby + Node.js (TypeScript) API</h1>
+      <h1>Phucie and Oanh 2020</h1>
       <h2>
-        Deployed with{' '}
-        <a
-          href="https://zeit.co/docs"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          ZEIT Now
-        </a>
-        !
+        Date idea #26: Attend an Outdoor Concert
       </h2>
       <p>
-        <a
-          href="https://github.com/zeit/now-examples/blob/master/gatsby-functions"
-          target="_blank"
-          rel="noreferrer noopener"
-        >
-          This project
-        </a>{' '}
-        is a <a href="https://www.gatsbyjs.org/">Gatsby</a> app with two
-        directories, <code>/src</code> for static content and <code>/api</code>{' '}
-        which contains a serverless{' '}
-        <a href="https://nodejs.org/en/">Node.js (TypeScript)</a> function. See{' '}
-        <a href="/api/date">
-          <code>api/date</code> for the Date API with Node.js (TypeScript)
-        </a>
-        .
+      This one's a definite time commitment, so you might want to get to know each other over a drink or meal before going for this option. But the perks of an outdoor show, especially if you both love the band, are the good vibes. If there's space on
       </p>
       <br />
-      <h2>The date according to Node.js (TypeScript) is:</h2>
-      <p>{date ? date : 'Loading date...'}</p>
+      <h2>The date is:</h2>
+      <p>{dateIdea ? dateIdea : 'Loading date idea...'}</p>
     </main>
   );
 }
